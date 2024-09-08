20514f40f0356eb6e2d9ec71322d35a681700dcb
lea0101 <lalsatie@purdue.edu>
2024-09-08 15:16:45 -0400

    Initial commit

---
+++ lea0101.github.io/Program.cs
@@ -0,0 +1,37 @@
+using MudBlazor.Services;
+using lea0101.github.io.Client.Pages;
+using lea0101.github.io.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(lea0101.github.io.Client._Imports).Assembly);
+
+app.Run();
